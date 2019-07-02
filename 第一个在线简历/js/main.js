eb5d760709103d2c96b90d6f86a0ad027ef5e1b8
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-02 21:47:10 +0800

    加入了滚动高亮，下拉弹出

--- 第一个在线简历/js/main.js
+++ 第一个在线简历/js/main.js
@@ -17,7 +17,6 @@ for (let i = 0; i < lis.length; ++i) {
                 continue;
             }
         }
-        // console.log(ul);
         ul.classList.add("active");
     }
     lis[i].onmouseleave = function (x) {
@@ -33,7 +32,6 @@ for (let i = 0; i < lis.length; ++i) {
                 continue;
             }
         }
-        // console.log(ul);
         ul.classList.remove("active");
     }
 }
@@ -50,7 +48,7 @@ for (let i = 0; i < aTags.length; ++i) {
         // let i = 0;
         // let duration = 500 / n;//0.5s内滚动25次，一次滚多少秒
         let currentPosition = scrollY;
-        let destination = jumpLocation.offsetTop -80;//跳转的地方的Y值
+        let destination = jumpLocation.offsetTop - 80;//跳转的地方的Y值
         let distance = destination - currentPosition;
         // let step = distance / n;//每次滚多远
         // let id = setInterval(() => {
@@ -62,17 +60,15 @@ for (let i = 0; i < aTags.length; ++i) {
         //     window.scrollTo(0, currentPosition + step * i);
         // }, duration);
 
-
-
         // Setup the animation loop.
         function animate(time) {
             requestAnimationFrame(animate);
             TWEEN.update(time);
         }
         requestAnimationFrame(animate);
-        let t= Math.abs(distance/100*300);//100px用300ms的时间
-        if(t>500){
-            t=500;//超过500ms，不管，最多500ms的时间
+        let t = Math.abs(distance / 100 * 300);//100px用300ms的时间
+        if (t > 500) {
+            t = 500;//超过500ms，不管，最多500ms的时间
         }
         const coords = { y: currentPosition }; // Start at (0, 0)
         const tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
@@ -83,8 +79,49 @@ for (let i = 0; i < aTags.length; ++i) {
                 window.scrollTo(0, coords.y);
             })
             .start(); // Start the tween immediately.
-
-
     }
 }
+siteWelcome.classList.remove("active")
 
+let specialTags = document.querySelectorAll('[data-x]');//需要滚动高亮的元素
+
+function getMinIndex(){
+    let minIndex = 0;
+    for(let i = 0;i<specialTags.length;++i){
+        if(Math.abs(specialTags[i].offsetTop - scrollY) < Math.abs(specialTags[minIndex].offsetTop - scrollY)){
+            minIndex = i;
+        }
+        // specialTags[i].classList.add("offset");
+    }
+    return minIndex;
+}
+for(let i = 0;i<specialTags.length;++i){
+    specialTags[i].classList.add("offset");
+}//刚开始的时候三个标签全部加上偏移量
+let minIndexAll = getMinIndex();
+window.setTimeout(function(){
+    specialTags[minIndexAll].classList.remove("offset");
+},500);
+//我在html里把第一个a标签加上.highlight了，所以还没滚动的时候第一个标签就高亮了
+
+
+
+document.onscroll = function () {
+    if (scrollY > 0) {
+        topNavBar.classList.add("sticky")
+    }
+    else {
+        topNavBar.classList.remove("sticky")
+    }
+    let minIndex = getMinIndex();
+    specialTags[minIndex].classList.remove("offset");
+    console.log(minIndex);
+    if(minIndex !== minIndexAll){//和上一个高亮的a标签不一样的
+        //就把上一个a标签取消高亮，当前的高亮
+        let aTagLast = document.querySelector('[href="#' + specialTags[minIndexAll].id +'"]');
+        aTagLast.classList.remove("highlight");
+        let aTagCurrent = document.querySelector('[href="#' + specialTags[minIndex].id +'"]');
+        aTagCurrent.classList.add("highlight")
+        minIndexAll = minIndex;
+    }
+}
