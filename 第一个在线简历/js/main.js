f2ea3482a48f47726edc5f44be1c5fcdab061680
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-02 15:11:37 +0800

    加入页面跳转功能

--- 第一个在线简历/js/main.js
+++ 第一个在线简历/js/main.js
@@ -10,14 +10,14 @@ for (let i = 0; i < lis.length; ++i) {
         // console.log(a.childNodes[3])
         for (let j = 0; j < a.childNodes.length; ++j) {
             if (a.childNodes[j].tagName === "UL") {
-                console.log(a.childNodes[j]);
+                // console.log(a.childNodes[j]);
                 ul = a.childNodes[j];//获取到这个li下面需要弹出的二级菜单ul
             }
             else {
                 continue;
             }
         }
-        console.log(ul);
+        // console.log(ul);
         ul.classList.add("active");
     }
     lis[i].onmouseleave = function (x) {
@@ -26,14 +26,26 @@ for (let i = 0; i < lis.length; ++i) {
         // console.log(a.childNodes[3])
         for (let j = 0; j < a.childNodes.length; ++j) {
             if (a.childNodes[j].tagName === "UL") {
-                console.log(a.childNodes[j]);
+                // console.log(a.childNodes[j]);
                 ul = a.childNodes[j];//获取到这个li下面需要弹出的二级菜单ul
             }
             else {
                 continue;
             }
         }
-        console.log(ul);
+        // console.log(ul);
         ul.classList.remove("active");
+    }
+}
+console.log(siteSkills.offsetTop)
+let aTags = document.querySelectorAll(".nav>ul>li>a");
+console.log(aTags);
+for(let i =0;i<aTags.length;++i){
+    aTags[i].onclick = (e) => {
+        e.preventDefault();
+        let temp = e.currentTarget; 
+        let jumpLocation = document.querySelector(temp.getAttribute("href"));
+        console.log(jumpLocation);
+        window.scrollTo(0, jumpLocation.offsetTop - 80);
     }
 }
