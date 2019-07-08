86a0587b8b86a096dc4f9767074890a1b465cf88
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-08 22:03:07 +0800

    自己动手写简版jQuery

---
+++ 自己动手写简版jquery/main.js
@@ -0,0 +1,131 @@
+function getSiblings(node) {
+    let children = node.parentNode.children;
+    let siblings = { length: 0 };
+    for (let i = 0; i < children.length; ++i) {
+        if (children[i] !== node) {
+            siblings[siblings.length++] = children[i];
+        }
+    }
+    return siblings;
+}
+//[a:true,b:true,c:false]
+//{a:true.b:false,c:true}
+function addClass(node, classes) {
+    let temp = { true: "add", false: "remove" };
+    for (key in classes) {
+        node.classList[temp[classes[key]]](key);
+    }
+    // classes.forEach(value => node.classList.add(value));
+}
+// console.log(getSiblings(x));
+// addClass(x, {a:true,b:false,c:true});
+var fan = {}
+fan.getSiblings = function (node) {
+    let children = node.parentNode.children;
+    let siblings = { length: 0 };
+    for (let i = 0; i < children.length; ++i) {
+        if (children[i] !== node) {
+            siblings[siblings.length++] = children[i];
+        }
+    }
+    return siblings;
+}
+fan.addClass = function (node, classes) {
+    let temp = { true: "add", false: "remove" };
+    for (key in classes) {
+        node.classList[temp[classes[key]]](key);
+    }
+}
+// console.log(fan.getSiblings(x));
+// fan.addClass(x,{a:true,b:false,c:true});
+Node.prototype.getSiblings = function () {
+    let children = this.parentNode.children;
+    let siblings = { length: 0 };
+    for (let i = 0; i < children.length; ++i) {
+        if (children[i] !== this) {
+            siblings[siblings.length++] = children[i];
+        }
+    }
+    return siblings;
+}
+Node.prototype.addClass = function (classes) {
+    let temp = { true: "add", false: "remove" };
+    for (key in classes) {
+        this.classList[temp[classes[key]]](key);
+    }
+}
+// console.log(x.getSiblings.call(x));
+// x.addClass.call(x,{a:true,b:false,c:true});
+// function jQuery(node) {
+//     return {
+//         getSiblings: function () {
+//             let children = node.parentNode.children;
+//             let siblings = { length: 0 };
+//             for (let i = 0; i < children.length; ++i) {
+//                 if (children[i] !== node) {
+//                     siblings[siblings.length++] = children[i];
+//                 }
+//             }
+//             return siblings;
+//         },
+//         addClass:function(classes){
+//             let temp = { true: "add", false: "remove" };
+//             for (key in classes) {
+//             node.classList[temp[classes[key]]](key);
+//             }
+//         }
+//     }
+// }
+// window.$ = jQuery;
+// var node = $(x);
+// console.log(node.getSiblings.call(undefined));
+// node.addClass.call(undefined,{a:true,b:false,c:true});
+
+//{a:true, b:false, c:true}
+function jQuery(nodeOrSelector) {
+    return {
+        addClass: function(classes){
+            let temp = {true: "add", false: "remove"};
+            if(typeof nodeOrSelector === "string"){
+                let nodes = document.querySelectorAll(nodeOrSelector);
+                for(let i = 0;i<nodes.length;++i){
+                    for(key in classes){
+                        nodes[i].classList[temp[classes[key]]](key);
+                    }
+                }
+            }
+            else if(nodeOrSelector instanceof Node){
+                for(key in classes){
+                    nodeOrSelector.classList[temp[classes[key]]](key);
+                }
+            }
+        },
+        text: function(newText){
+            let arr = [];
+            if(typeof nodeOrSelector === "string"){
+                let nodes = document.querySelectorAll(nodeOrSelector);
+                for(let i = 0;i<nodes.length;++i){
+                    if(newText === undefined)
+                        arr.push(nodes[i].textContent);
+                    else
+                        nodes[i].textContent = newText;                                     
+                }
+            }
+            else if(nodeOrSelector instanceof Node){
+                if(newText === undefined)
+                    arr.push(nodeOrSelector.textContent);
+                else
+                    arr.push(nodeOrSelector.textContent);                
+            }
+            return arr;
+        }
+    }
+}
+window.$ = jQuery;
+var node = $("ul>li");
+console.log(node);
+node.addClass({a:true, b:false, c:true});
+console.log(node.text("222"));
+console.log(node.text());
+// console.log(node.getSiblings.call(undefined));
+// node.addClass.call(undefined,{a:true,b:false,c:true});
