6a87116bd60f6675e0dcd84022200e5a8a9d5e21
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-10 20:10:38 +0800

    解决了切页面会出现的bug

--- 自己做一个轮播图/无缝轮播/main.js
+++ 自己做一个轮播图/无缝轮播/main.js
@@ -1,16 +1,27 @@
 let size = $(".images-wrapper>img").length;
 let index =1;
 init(getImage(index));
-setInterval(() => {
+// let timer;
+let timer = setInterval(() => {
     makeLeave(getImage(index));
     //我这里把makeLeave和makeQueue合起来了，makeQueue是包含在makeLeave里的
     //还有一种思路就是makeLeave和makeQueue就是单纯的，谁也不包含谁，makeLeave结束再让当前图片makeQueue
     makeCurrent(getImage(index+1));
     index++;
-}, 3000);
-//无缝轮播到底为止，就这么一点
-
-
+}, 2000);
+$(document).on("visibilitychange",()=>{
+    if(document.hidden){
+        clearInterval(timer);
+    }else{
+        timer = setInterval(() => {
+            makeLeave(getImage(index));
+            //我这里把makeLeave和makeQueue合起来了，makeQueue是包含在makeLeave里的
+            //还有一种思路就是makeLeave和makeQueue就是单纯的，谁也不包含谁，makeLeave结束再让当前图片makeQueue
+            makeCurrent(getImage(index+1));
+            index++;
+        }, 2000);
+    }   
+});
 
 
 //下面的都是函数，可以不看
@@ -19,6 +30,7 @@ function init($img1){
 }
 function makeCurrent($img){
     return $img.removeClass("queue leave").addClass("current");
+    //removeClass()什么都不加就是移除所有类
 }
 function makeLeave($img){
     $img.removeClass("current queue").addClass("leave").
