c2914466f4e6ec86b61ec22d9bd8ed3b17e34821
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-10 11:43:06 +0800

    增加了透明度，写了几条注释

--- 自己做一个轮播图/无缝轮播/main.js
+++ 自己做一个轮播图/无缝轮播/main.js
@@ -1,19 +1,19 @@
 let size = $(".images-wrapper>img").length;
-console.log(size);
 let index =1;
-let $img1 = $(".images-wrapper>img:nth-child(1)");
-let $img2 = $(".images-wrapper>img:nth-child(2)");
-init($(`.images-wrapper>img:nth-child(${index})`));
-// setTimeout(()=>{
-//     makeLeave($img1);
-//     makeCurrent($img2);
-// },3000)
+init(getImage(index));
 setInterval(() => {
-    makeLeave($(`.images-wrapper>img:nth-child(${mod(index)})`));
-    makeCurrent($(`.images-wrapper>img:nth-child(${mod(index+1)})`));
+    makeLeave(getImage(index));
+    //我这里把makeLeave和makeQueue合起来了，makeQueue是包含在makeLeave里的
+    //还有一种思路就是makeLeave和makeQueue就是单纯的，谁也不包含谁，makeLeave结束再让当前图片makeQueue
+    makeCurrent(getImage(index+1));
     index++;
 }, 3000);
+//无缝轮播到底为止，就这么一点
 
+
+
+
+//下面的都是函数，可以不看
 function init($img1){
     $img1.addClass("current").siblings().addClass("queue");
 }
@@ -22,7 +22,7 @@ function makeCurrent($img){
 }
 function makeLeave($img){
     $img.removeClass("current queue").addClass("leave").
-    one("transitionend", function(e){
+    one("transitionend", function(){
         // $(this).removeClass("leave").addClass("queue");
         makeQueue($img);
     });
@@ -33,4 +33,7 @@ function makeQueue($img){
 }
 function mod(x){
     return x%size === 0 ? size : x % size;
+}
+function getImage(n){
+    return $(`.images-wrapper>img:nth-child(${mod(n)})`);
 }
