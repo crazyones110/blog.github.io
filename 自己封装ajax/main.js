463718b3ab7e36bb0fdc733624b434a2350efc65
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-16 17:17:32 +0800

    加入了解构赋值

--- 自己封装ajax/main.js
+++ 自己封装ajax/main.js
@@ -13,26 +13,27 @@ window.jQuery = function (nodeOrSelector) {
     return nodes;
 }
 window.$ = window.jQuery;
-window.jQuery.ajax = (method, url, payload, successFn, failFn) => {
+window.jQuery.ajax = function({url,method, body, success, fail}){
     let request = new XMLHttpRequest();
     request.open(method, url);
-    request.onload = (e) => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined, request.responseText);
-            } else if (request.status >= 300) {
-                failFn.call(undefined, request);
+    request.send(body);
+    request.addEventListener("load", function(){
+        if(request.readyState === 4){
+            if(request.status >=200 && request.status < 300){
+                success.call(undefined, request.responseText);
+            }else if(request.status >= 300){
+                fail.call(undefined, request);
             }
         }
-    }
-    request.send(payload);
+    })
 }
-button.onclick = (e)=>{
-    $.ajax(
-        "post",
-        "/xxx",
-        "a=3&b=5",
-        (responseText)=>{},
-        (request)=>{}
-    );
-}
+let obj = {a:1,b:2};
+button.addEventListener("click",()=>{
+    jQuery.ajax({
+        url:"/xxx",
+        method:"post",
+        body:JSON.stringify(obj),
+        success:(responseText)=>{console.log(responseText)},
+        fail:(request)=>{console.log(request)},
+    })
+});
