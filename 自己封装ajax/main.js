bafa9d8c59c0d36886579d8279b18643f70c3d2a
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-16 17:18:55 +0800

    加入了Promise，支持then的调用

--- 自己封装ajax/main.js
+++ 自己封装ajax/main.js
@@ -14,18 +14,20 @@ window.jQuery = function (nodeOrSelector) {
 }
 window.$ = window.jQuery;
 window.jQuery.ajax = function({url,method, body, success, fail}){
-    let request = new XMLHttpRequest();
-    request.open(method, url);
-    request.send(body);
-    request.addEventListener("load", function(){
-        if(request.readyState === 4){
-            if(request.status >=200 && request.status < 300){
-                success.call(undefined, request.responseText);
-            }else if(request.status >= 300){
-                fail.call(undefined, request);
+    return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        request.open(method, url);
+        request.send(body);
+        request.addEventListener("load", function(){
+            if(request.readyState === 4){
+                if(request.status >=200 && request.status < 300){
+                    resolve.call(undefined, request.responseText);
+                }else if(request.status >= 300){
+                    reject.call(undefined, request);
+                }
             }
-        }
-    })
+        })
+    });
 }
 let obj = {a:1,b:2};
 button.addEventListener("click",()=>{
@@ -33,7 +35,9 @@ button.addEventListener("click",()=>{
         url:"/xxx",
         method:"post",
         body:JSON.stringify(obj),
-        success:(responseText)=>{console.log(responseText)},
-        fail:(request)=>{console.log(request)},
-    })
+    }).then((responseText)=>{
+        console.log(responseText);
+    },(request)=>{
+        console.log(request);
+    });
 });
