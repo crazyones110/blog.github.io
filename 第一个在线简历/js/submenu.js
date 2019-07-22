0b17c91b501dbbf3471873efe1a5c354a70a10de
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-22 19:53:25 +0800

    采用MVC思想封装代码

--- 第一个在线简历/js/submenu.js
+++ 第一个在线简历/js/submenu.js
@@ -1,40 +1,45 @@
 !function () {
-    let lis = document.getElementsByClassName("submenu");//好几个li
-    let uls = document.querySelectorAll(".submenu>ul")
-    // for(let i =0;i<uls.length;++i){
-    //     console.log(uls[i])
-    // }
-    for (let i = 0; i < lis.length; ++i) {
-        lis[i].onmouseenter = function (x) {
-            let a = x.currentTarget//a是包着menu和submenu的大li
-            let ul;
-            // console.log(a.childNodes[3])
-            for (let j = 0; j < a.childNodes.length; ++j) {
-                if (a.childNodes[j].tagName === "UL") {
-                    // console.log(a.childNodes[j]);
-                    ul = a.childNodes[j];//获取到这个li下面需要弹出的二级菜单ul
+    var view = document.getElementsByClassName("submenu");//好几个li
+    var controller = {
+        view: null,
+        init: function(view){
+            this.view = view;
+            this.bindEvents();
+        },
+        bindEvents: function(){
+            for (let i = 0; i < this.view.length; ++i) {
+                this.view[i].onmouseenter = function (x) {
+                    let a = x.currentTarget//a是包着menu和submenu的大li
+                    let ul;
+                    // console.log(a.childNodes[3])
+                    for (let j = 0; j < a.childNodes.length; ++j) {
+                        if (a.childNodes[j].tagName === "UL") {
+                            // console.log(a.childNodes[j]);
+                            ul = a.childNodes[j];//获取到这个li下面需要弹出的二级菜单ul
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    ul.classList.add("active");
                 }
-                else {
-                    continue;
+                this.view[i].onmouseleave = function (x) {
+                    let a = x.currentTarget//a是包着menu和submenu的大li
+                    let ul;
+                    // console.log(a.childNodes[3])
+                    for (let j = 0; j < a.childNodes.length; ++j) {
+                        if (a.childNodes[j].tagName === "UL") {
+                            // console.log(a.childNodes[j]);
+                            ul = a.childNodes[j];//获取到这个li下面需要弹出的二级菜单ul
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                    ul.classList.remove("active");
                 }
             }
-            ul.classList.add("active");
-        }
-        lis[i].onmouseleave = function (x) {
-            let a = x.currentTarget//a是包着menu和submenu的大li
-            let ul;
-            // console.log(a.childNodes[3])
-            for (let j = 0; j < a.childNodes.length; ++j) {
-                if (a.childNodes[j].tagName === "UL") {
-                    // console.log(a.childNodes[j]);
-                    ul = a.childNodes[j];//获取到这个li下面需要弹出的二级菜单ul
-                }
-                else {
-                    continue;
-                }
-            }
-            ul.classList.remove("active");
-        }
+        },
     }
-
+    controller.init(view);
 }.call()
