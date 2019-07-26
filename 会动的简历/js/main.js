1ed423b7562ce518268e6e8b692671b38bf6361b
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-07-26 16:32:00 +0800

    加入白纸

--- 会动的简历/js/main.js
+++ 会动的简历/js/main.js
@@ -1,61 +1,86 @@
-var n = 1;
 var str = `
-        /*
-        面试官你好，我是范路平
-        我将以动画的形式介绍我自己
+  /*
+  面试官你好，我是范路平
+  我将以动画的形式介绍我自己
 
-        只用文字介绍太单调了
-        我就用代码来介绍吧
+  只用文字介绍太单调了
+  我就用代码来介绍吧
 
-        首先准备一些样式
-        */
+  首先准备一些样式
+  */
 
-        *{
-            transition: all 1s;
-        }
-        html{
-            background: rgb(222, 222 ,222);
-            font-size: 16px;
-        }
-        #code{
-            border: 1px solid red;
-            padding: 16px;
-        }
-        /* 接下来我要加入代码高亮 */
-        
-        /* 加点动画吧 */
-        #code{
-          animation:swoop 1s ease-in-out;
-          animation-fill-mode: forwards;
-          animation-iteration-count: 1;
-        }
-        @keyframes swoop{
-          0%{
-            transform: scale(0.5);
-          }
-          70%{
-            transform: scale(1.2);
-          }
-          100%{
-            transform: scale(1);
-          }
-        }
-
-        /* 不玩了，我来介绍一下我自己吧 */
-        `
-var result = Prism.highlight(str, Prism.languages.css, 'css')
-// console.log(result)
-console.log(typeof result);
+  *{
+      transition: all 1s;
+  }
+  html{
+      background: rgb(222, 222 ,222);
+      font-size: 16px;
+  }
+  #code{
+      border: 1px solid red;
+      padding: 16px;
+  }
+  /* 接下来我要加入代码高亮 */
+  .token.selector{
+    color: #690;
+  }       
+  .token.property{
+    color: #905
+  }  
+  .token.function{
+    color: #dd4a68;
+  }
+  .token.atrule{
+    color: #07a;
+  }          
+  .token.comment{
+    color: slategray;
+  } 
+  
+  /* 加点动画吧 */
+  #code{
+    
+  }
+  /* 不玩了，我来介绍一下我自己吧 */
+  /* 我需要一张白纸 */
+  `
+var n = 1;
 var interval = setInterval(() => {
 
   // Returns a highlighted HTML string
 
   code.innerHTML = str.substring(0, n)
-  // code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
+  code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
   styleSheet.innerHTML = str.substring(0, n)
   n++;
   if (n > str.length) {
     clearInterval(interval)
-    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
+    addPaper()
+    displayPaper(str)
   }
 }, 10)
+function addPaper() {
+  var paper = document.createElement("div")
+  paper.id = "paper"
+  document.body.appendChild(paper)
+}
+let n2 = 1
+function displayPaper(prevStr) {
+  var str2 = `
+  #paper{
+    width: 200px;
+    height: 200px;
+    background-color: red;
+  }
+  `
+  var interval2 = setInterval(() => {
+    code.innerHTML = prevStr
+    code.innerHTML = code.innerHTML + str2.substring(0, n2)
+    styleSheet.innerHTML = code.innerHTML
+    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
+    n2++
+    if(n2 > str2.length){
+      clearInterval(interval2)
+    }
+  }, 10);
+}
