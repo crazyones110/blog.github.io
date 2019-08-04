de0279062a10923fad3016924a3da846a2d3e13d
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-08-04 22:28:07 +0800

    代码显示

--- 动画版画一个皮卡丘/js/main.js
+++ 动画版画一个皮卡丘/js/main.js
@@ -1,3 +1,18 @@
 !function(){
-    
+    function writeCode(prefix, str, fn){
+        let pre = document.querySelector(".codeWrapper>pre")
+        let styleTag = document.querySelector("#styleTag")
+        let n =1
+        let interval = setInterval(() => {
+            pre.innerHTML = prefix + str.substring(0, n)
+            styleTag.innerHTML = prefix + str.substring(0, n)
+            pre.scrollTop = pre.scrollHeight
+            n++
+            if(n > str.length){
+                window.clearInterval(interval)
+                fn && fn()
+            }
+        }, 10);
+    }
+    writeCode("", "123213124sf a asd asd ")
 }.call()
